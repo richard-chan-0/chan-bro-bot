88449709ba49f7613a0910a5f9f9be55219fe71f
Richard Chan <r32095@gmail.com>
2022-11-07 23:14:48 -0600

    added my first discord bot

---
+++ README.md
@@ -0,0 +1,2 @@
+tutorial: https://realpython.com/how-to-make-a-discord-bot-python/#connecting-a-bot
+documentation: https://discordpy.readthedocs.io/en/stable/index.html
---
+++ bot.py
@@ -0,0 +1,36 @@
+import os
+from discord import Intents
+from discord.ext import commands
+from dotenv import load_dotenv
+import random
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} has connected to Discord!!")
+
+
+@bot.command(name="99")
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        "I'm the human form of the 💯 emoji.",
+        "Bingpot!",
+        (
+            "Cool. Cool cool cool cool cool cool cool, "
+            "no doubt no doubt no doubt no doubt."
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+
+bot.run(TOKEN)
