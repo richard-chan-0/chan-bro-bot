6af1ee5893454234ad73a1e8d6fa1147896fb209
Richard Chan <r32095@gmail.com>
2022-11-14 23:50:50 -0600

    added urban dictionary definition api

--- bot.py
+++ bot.py
@@ -2,7 +2,9 @@ import os
 from discord import Intents
 from discord.ext import commands
 from dotenv import load_dotenv
-import random
+from requests import request
+import json
+from random import sample
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -18,19 +20,34 @@ async def on_ready():
     print(f"{bot.user.name} has connected to Discord!!")
 
 
-@bot.command(name="99")
-async def nine_nine(ctx):
-    brooklyn_99_quotes = [
-        "I'm the human form of the 💯 emoji.",
-        "Bingpot!",
-        (
-            "Cool. Cool cool cool cool cool cool cool, "
-            "no doubt no doubt no doubt no doubt."
-        ),
-    ]
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
 
-    response = random.choice(brooklyn_99_quotes)
-    await ctx.send(response)
+
+@bot.command(name="ud")
+async def urban_dictionary(ctx, arg):
+    urban_url = os.getenv("URBAN_DICTIONARY_URL")
+    query = {"term": arg}
+    headers = {
+        "X-RapidAPI-Key": "d2a33b43c8mshf876d903bee8ee9p198929jsn920fa3852e7a",
+        "X-RapidAPI-Host": "mashape-community-urban-dictionary.p.rapidapi.com",
+    }
+    try:
+        response = request("GET", urban_url, headers=headers, params=query)
+        responses = json.loads(response.text)["list"]
+        num_samples = 3 if len(responses) else len(responses)
+        indexes = sample(range(len(responses)), num_samples)
+        for index in indexes:
+            response = responses[index]
+            definition = response["definition"]
+            author = response["author"]
+            await ctx.send(f"{author} says '{arg}' means '{definition}'")
+
+    except TypeError as err:
+        await ctx.send("noo...you know ask me properly...")
+    finally:
+        await ctx.send(f"why did you search for '{arg}' on urban dictionary!!")
 
 
 bot.run(TOKEN)
