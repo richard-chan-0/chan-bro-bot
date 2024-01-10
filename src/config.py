47ea652899153b7d63bdd7f6c8c8a7a806bb2550
Richard Chan <r32095@gmail.com>
2024-01-10 00:11:27 -0600

    first discord task

--- src/config.py
+++ src/config.py
@@ -6,9 +6,18 @@ from os import getenv
 class Config:
     api_key: str
     discord_key: str
+    general_channel_id: str
+    video_channel_id: str
 
 
 API_KEY = getenv("RAPID_API_KEY")
 TOKEN = getenv("DISCORD_TOKEN")
+GENERAL = getenv("GENERAL_CHANNEL_ID")
+VIDEO = getenv("VIDEO_CHANNEL_ID")
 
-config = Config(api_key=API_KEY, discord_key=TOKEN)
+config = Config(
+    api_key=API_KEY,
+    discord_key=TOKEN,
+    general_channel_id=GENERAL,
+    video_channel_id=VIDEO,
+)
