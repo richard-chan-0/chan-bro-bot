aaee2bcf4c34e94e73e475dba10ba36de892c811
Richard Chan <r32095@gmail.com>
2024-01-17 21:06:28 -0600

    completed task

---
+++ src/cogs/notification/notification.py
@@ -0,0 +1,23 @@
+from discord.ext import commands, tasks
+from src.config import Config
+import datetime as dt
+from random import randint
+
+night = dt.time(hour=11, minute=30)
+
+
+class NotificationCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot: commands.Bot = bot
+
+    def get_night_message(self):
+        messages = ["good night!", "晚安！"]
+        message_index = randint(0, len(messages))
+        return messages[message_index]
+
+    @tasks.loop(time=night)
+    async def good_night_notification(self):
+        channel_id = int(Config.general_channel_id)
+        channel = self.bot.get_channel(channel_id)
+
+        await channel.send(self.get_night_message())
