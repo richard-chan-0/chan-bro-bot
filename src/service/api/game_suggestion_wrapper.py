18733a9a79dcbb22816df76dbed82622014a73b2
Richard Chan <r32095@gmail.com>
2025-07-02 14:32:26 -0500

    add steam ids file reading and organize files into service folder

---
+++ src/service/api/game_suggestion_wrapper.py
@@ -0,0 +1,15 @@
+from requests import get
+from logging import getLogger
+from src.lib.exceptions import GameSuggestionExcepion
+
+logger = getLogger(__name__)
+
+
+async def get_shared_games() -> dict:
+    try:
+        logger.info("sending api request")
+        api_response = get(url="localhost:5010")
+        return api_response.json()
+    except Exception as err:
+        logger.error(err)
+        raise GameSuggestionExcepion(err)
